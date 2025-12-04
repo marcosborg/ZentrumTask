600cf19f47f4bac3bed2daa542d3dfd4c654fb0f
Marcos Borges <110690829+marcosborg@users.noreply.github.com>
2025-12-04 18:26:11 +0000

    modified:   routes/web.php

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,4 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [WebsiteController::class, 'index']);
 Route::post('/contact', [WebsiteController::class, 'storeContact'])->name('contact.submit');
+Route::get('/cms/{page}/{slug?}', [WebsiteController::class, 'showCms'])->name('cms.show');
