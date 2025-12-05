21f973c3bf7e767efe351bda57843411dad6c94c
Marcos Borges <110690829+marcosborg@users.noreply.github.com>
2025-12-05 07:38:43 +0000

    Update meta

--- resources/views/website/index.blade.php
+++ resources/views/website/index.blade.php
@@ -1,5 +1,10 @@
 @extends('website.layout')
 
+@push('head')
+  <title>Zentrum TVDE — Apoio Completo para Motoristas TVDE em Portugal</title>
+  <meta name="description" content="A Zentrum TVDE ajuda novos e experientes motoristas com formação, documentação, gestão de viaturas e suporte contínuo. Trabalha TVDE com transparência e confiança.">
+@endpush
+
 @section('content')
 
     <x-hero />
